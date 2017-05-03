fc59b97249abb5e71cd3086db73114ce5efaa272
xjhaobang <912091618@qq.com>
2017-05-03 19:45:25 +0800

    一点点审核者界面，用了ViewPager，BaseActivity和BaseFragment的应用可以参考下VerifyActivity和Fragment

---
+++ app/src/main/java/com/rdc/gdut_activity/fragment/VerifyFragment.java
@@ -0,0 +1,40 @@
+package com.rdc.gdut_activity.fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.rdc.gdut_activity.R;
+import com.rdc.gdut_activity.base.BaseFragment;
+
+
+public class VerifyFragment extends BaseFragment {
+    private static final String TAG = "VerifyFragment";
+    private String mType; //Fragment类型标志，如审核和未审核
+
+    /**
+     *创建Fragment，并传入参数
+     */
+    public static VerifyFragment newInstance(String type){
+        VerifyFragment verifyFragment = new VerifyFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("fragmentType",type);
+        verifyFragment.setArguments(bundle);
+        return verifyFragment;
+    }
+
+    @Override
+    protected int setLayoutResourceId() {
+        return R.layout.fragment_verify;
+    }
+
+    @Override
+    protected void initData(Bundle bundle) {
+        mType = bundle.getString("fragmentType");
+        Log.e(TAG, "initData: "+ mType);
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+    }
+}
